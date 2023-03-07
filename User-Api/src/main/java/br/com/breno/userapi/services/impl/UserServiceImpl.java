0673e244cba543c50c84bef076d0a282ac3dac43
breno.lima <brenoryan21@hotmail.com>
2023-03-07 10:57:19 -0300

    feat: configuring microservice to serve the eureka server

--- User-Api/src/main/java/br/com/breno/userapi/services/impl/UserServiceImpl.java
+++ User-Api/src/main/java/br/com/breno/userapi/services/impl/UserServiceImpl.java
@@ -4,19 +4,29 @@ import br.com.breno.userapi.domain.User;
 import br.com.breno.userapi.repositories.UserRepository;
 import br.com.breno.userapi.services.UserService;
 import br.com.breno.userapi.services.exceptions.ObjectNotFoundException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository repository;
+    private final Environment envi;
 
     @Override
     public User findById(Long id) {
+
+        log.info("PAYROLL_SERVICE ::: Get request on "
+                + envi.getProperty("local.server.port") + " port" );
+
         return repository.findById(id).orElseThrow(() ->
                 new ObjectNotFoundException("Object not found"));
     }
